52a4d68785cb34011dbe98f39538e3f4a6d15a79
samrat malla <samratmalla48@gmail.com>
2023-08-01 00:11:12 +1000

    connected to database and used MORGAN for debug

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-import express from "express"
-import colors from"colors"
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import path from "path";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
+// configure dotenv file
+dotenv.config();
+// database config
+connectDB();
 // rest object
+
 const app = express();
+// middleware
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+const PORT=process.env.PORT|| 8080;
 // rest api
 app.get("/", (req, res) => {
   res.send({
     message: "welcome to ecommerce app",
   });
 });
-const PORT = 8080;
-app.listen(PORT,() => {
-    console.log(`running on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
 });
